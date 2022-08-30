5135cc80ea70188d1850002853081131b28c0114
Fabian Schlieper <fabian@schlieper.email>
2022-08-30 11:00:49 +0100

    FuturesPool add tuple names

--- bmslib/__init__.py
+++ bmslib/__init__.py
@@ -1,13 +1,18 @@
 import asyncio
-from typing import Dict
+from typing import Dict, Union, Tuple, Any
 
+# NameType = Union[str, Tuple[str]]
+NameType = Union[str, int, Tuple[Union[str, int]]]
 
 class FuturesPool:
 
     def __init__(self):
-        self._futures: Dict[int, asyncio.Future] = {}
+        self._futures: Dict[str, asyncio.Future] = {}
 
-    def acquire(self, name):
+    def acquire(self, name: NameType):
+        if isinstance(name, tuple):
+            return tuple(self.acquire(n) for n in name)
+
         assert name not in self._futures, "already waiting for %s" % name
         fut = asyncio.Future()
         self._futures[name] = fut
@@ -23,9 +28,13 @@ class FuturesPool:
             fut.cancel()
         self._futures.clear()
 
-    async def wait_for(self, name, timeout):
+    async def wait_for(self, name: NameType, timeout):
+        if isinstance(name, tuple):
+            tasks = [self.wait_for(n, timeout) for n in name]
+            return await asyncio.gather(*tasks, return_exceptions=False)
+
         try:
             return await asyncio.wait_for(self._futures[name], timeout)
-        except asyncio.TimeoutError:
+        except (asyncio.TimeoutError, asyncio.CancelledError):
             del self._futures[name]
             raise
