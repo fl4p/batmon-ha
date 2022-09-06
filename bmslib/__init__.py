4f9da274532024bffe6c74da0602500848c61742
Fabian Schlieper <fabian@schlieper.email>
2022-09-06 12:01:12 +0100

    futureContext

--- bmslib/__init__.py
+++ bmslib/__init__.py
@@ -1,8 +1,9 @@
 import asyncio
-from typing import Dict, Union, Tuple, Any
+from typing import Dict, Union, Tuple
 
 # NameType = Union[str, Tuple[str]]
 NameType = Union[str, int, Tuple[Union[str, int]]]
+
 
 class FuturesPool:
 
@@ -11,12 +12,13 @@ class FuturesPool:
 
     def acquire(self, name: NameType):
         if isinstance(name, tuple):
-            return tuple(self.acquire(n) for n in name)
+            tuple(self.acquire(n) for n in name)
+            return FutureContext(name, pool=self)
 
         assert name not in self._futures, "already waiting for %s" % name
         fut = asyncio.Future()
         self._futures[name] = fut
-        return fut
+        return FutureContext(name, pool=self)
 
     def set_result(self, name, value):
         fut = self._futures.pop(name, None)
@@ -28,6 +30,11 @@ class FuturesPool:
             fut.cancel()
         self._futures.clear()
 
+    def remove(self, name):
+        if isinstance(name, tuple):
+            return tuple(self.remove(n) for n in name)
+        self._futures.pop(name, None)
+
     async def wait_for(self, name: NameType, timeout):
         if isinstance(name, tuple):
             tasks = [self.wait_for(n, timeout) for n in name]
@@ -36,5 +43,17 @@ class FuturesPool:
         try:
             return await asyncio.wait_for(self._futures[name], timeout)
         except (asyncio.TimeoutError, asyncio.CancelledError):
-            del self._futures[name]
+            self.remove(name)
             raise
+
+
+class FutureContext:
+    def __init__(self, name: Union[str, Tuple[str]], pool: FuturesPool):
+        self.name = name
+        self.pool = pool
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.pool.remove(self.name)
