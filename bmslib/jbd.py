a2395d85ed4243da6992bd3ae9bfc4b42be5897d
Fabian Schlieper <fabian@schlieper.email>
2022-10-23 23:16:44 +0200

    jbd switches

--- bmslib/jbd.py
+++ bmslib/jbd.py
@@ -1,13 +1,17 @@
 """
-
+JBD protocol references
+https://github.com/syssi/esphome-jbd-bms
+https://github.com/syssi/esphome-jbd-bms/blob/main/docs/Jiabaida.communication.protocol.pdf
+https://github.com/sshoecraft/jbdtool/blob/1168edac728d1e0bdea6cd4fa142548c445f80ec/main.c
 https://github.com/Bangybug/esp32xiaoxiangble/blob/master/src/main.cpp
 
+
+https://blog.ja-ke.tech/2020/02/07/ltt-power-bms-chinese-protocol.html # checksum
 Unseen:
 https://github.com/tgalarneau/bms
 
 """
 import asyncio
-from typing import Dict
 
 from bmslib import FuturesPool
 from .bms import BmsSample
@@ -27,10 +31,11 @@ class JbdBt(BtBms):
         super().__init__(address, **kwargs)
         self._buffer = bytearray()
         self._fetch_futures = FuturesPool()
+        self._switches = None
 
     def _notification_handler(self, sender, data):
 
-        #print("bms msg {0}: {1}".format(sender, data))
+        # print("bms msg {0}: {1}".format(sender, data))
         self._buffer += data
 
         if self._buffer.endswith(b'w'):
@@ -42,14 +47,18 @@ class JbdBt(BtBms):
             self._fetch_futures.set_result(command, buf)
 
     async def connect(self, **kwargs):
-        await super().connect(**kwargs)
+        try:
+            await super().connect(**kwargs)
+        except Exception as e:
+            self.logger.info("normal connect failed (%s), connecting with scanner", e)
+            await self._connect_with_scanner(**kwargs)
+
         await self.client.start_notify(self.UUID_RX, self._notification_handler)
 
     async def disconnect(self):
         await self.client.stop_notify(self.UUID_RX)
         self._fetch_futures.clear()
         await super().disconnect()
-
 
     async def _q(self, cmd):
         with self._fetch_futures.acquire(cmd):
@@ -88,6 +97,8 @@ class JbdBt(BtBms):
             # discharge_enabled
         )
 
+        self._switches = dict(sample.switches)
+
         # print(dict(num_cell=num_cell, num_temp=num_temp))
 
         # self.rawdat['P']=round(self.rawdat['Vbat']*self.rawdat['Ibat'], 1)
@@ -104,11 +115,43 @@ class JbdBt(BtBms):
         voltages = [(int.from_bytes(buf[4 + i * 2:i * 2 + 6], 'big')) for i in range(num_cell)]
         return voltages
 
+    async def set_switch(self, switch: str, state: bool):
+
+        assert switch in {"charge", "discharge"}
+
+        # see https://wiki.jmehan.com/download/attachments/59114595/JBD%20Protocol%20English%20version.pdf?version=1&modificationDate=1650716897000&api=v2
+        #
+        def jbd_checksum(cmd, data):
+            crc = 0x10000
+            for i in (data + bytes([len(data), cmd])):
+                crc = crc - int(i)
+            return crc.to_bytes(2, byteorder='big')
+
+        def jbd_message(status_bit, cmd, data):
+            return bytes([0xDD, status_bit, cmd, len(data)]) + data + jbd_checksum(cmd, data) + bytes([0x77])
+
+        if not self._switches:
+            await self.fetch()
+
+        new_switches = {**self._switches, switch: state}
+        switches_sum = sum(new_switches.values())
+        if switches_sum == 0:
+            tc = 0x00  # all on
+        elif switches_sum == 2:
+            tc = 0x03  # all off
+        elif switch == "charge" and not state:
+            tc = 0x01  # charge off
+        else:
+            tc = 0x02  # charge on, discharge off
+
+        data = jbd_message(status_bit=0x5A, cmd=0xE1, data=bytes([0x00, tc]))  # all off
+        self.logger.info("send switch msg: %s", data)
+        await self.client.write_gatt_char(self.UUID_TX, data=data)
 
 
 async def main():
-    mac_address = 'A3161184-6D54-4B9E-8849-E755F10CEE12'
-    # mac_address = 'A4:C1:38:44:48:E7'
+    # mac_address = 'A3161184-6D54-4B9E-8849-E755F10CEE12'
+    mac_address = 'A4:C1:38:44:48:E7'
     # serial_service = '0000ff00-0000-1000-8000-00805f9b34fb'
 
     bms = JbdBt(mac_address, name='jbd')
