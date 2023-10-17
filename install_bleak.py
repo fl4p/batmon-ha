2f40622be6446b90fbff8e9aeb48b2699d288563
Fabian <fabian@schlieper.email>
2023-10-17 20:07:14 +0100

    make influxdb requirement optional (fix https://github.com/fl4p/batmon-ha/issues/147)

--- install_bleak.py
+++ install_bleak.py
@@ -15,8 +15,18 @@ else:
     ver = '0.13.1a1'
     args = ['install', 'git+https://github.com/jpeters-ml/bleak@feature/windowsPairing']
 
+try:
+    import influxdb
+    influxdb_installed = True
+except ImportError:
+    influxdb_installed = False
+
+need_influxdb = bool(user_config.get('influxdb_host'))
+if need_influxdb:
+    args.append('influxdb')
+
 installed_ver = bleak_version()
-if installed_ver == ver:
+if installed_ver == ver and (influxdb_installed or not need_influxdb):
     sys.exit(0)
 
 logger.info(f'bleak {installed_ver} installed, want {ver}, running pip3 ' + ' '.join(args))
