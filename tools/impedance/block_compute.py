b91db95ded6d74690e4b0f4cf1430c1b9d0708c4
Fabian <fabian@schlieper.email>
2023-11-08 18:46:47 +0000

    datasets

--- tools/impedance/block_compute.py
+++ tools/impedance/block_compute.py
@@ -1,53 +1,50 @@
 import math
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
-
-from tools.impedance.datasets import ant24_2023_07
+import tools.impedance.datasets as datasets
 
 for ci in range(8):
-    df = ant24_2023_07(cell_index=ci)
+    # df =  datasets.ant24_2023_07(cell_index=ci)
+    df = datasets.batmon(device='bat_caravan', cell_index=0)
 
     block_size = 120
 
-    df = df.iloc[:int(len(df) /2)]
+    df = df.iloc[:int(len(df) / 2)]
 
     df = df.iloc[:len(df) - len(df) % block_size]
 
     import tools.impedance.ac_impedance
 
-    blocks = np.vsplit(df, int(len(df)/block_size))
+    blocks = np.vsplit(df, int(len(df) / block_size))
     results = []
 
     for b in blocks:
         t = b.index[-1]
         try:
             r, u0 = tools.impedance.ac_impedance.estimate(b.u, b.i)
-            results.append( (t, r, u0) )
+            results.append((t, r, u0))
         except Exception as e:
-            print('error %s at block %s' % (e, t))
+            # print('error %s at block %s' % (e, t))
+            pass
 
     results = pd.DataFrame(results, columns=['time', 'r', 'u0'])
     results.set_index("time", inplace=True)
-    #results.drop(columns="time", inplace=True)
-    print(results)
-    print("have estimate for %d/%d blocks" % (len(results),len(blocks)))
-    #results.r.plot(label='c%d R(q25)=%.2f' % (ci, results.r.quantile(.25)))
+    # results.drop(columns="time", inplace=True)
+    #print(results)
+    print("have estimate for %d/%d blocks" % (len(results), len(blocks)))
+    # results.r.plot(label='c%d R(q25)=%.2f' % (ci, results.r.quantile(.25)))
     plt.step(results.r.index, results.r.values, where='post', label='c%d R(q25)=%.2f' % (ci, results.r.quantile(.25)))
 
+# df.u.plot()
+# plt.show()
 
 
-#df.u.plot()
-#plt.show()
-
-
-#plt.semilogy()
+# plt.semilogy()
 
 plt.legend()
-plt.ylim((0.1,2))
+plt.ylim((0.1, 2))
 plt.grid()
 plt.show()
-
-
--- tools/impedance/datasets.py
+++ tools/impedance/datasets.py
@@ -28,3 +28,19 @@ def ant24_2023_07(cell_index=2):
     points.set_index(pd.DatetimeIndex(points.time), inplace=True)
     points.drop(columns='time', inplace=True)
     return points
+
+
+def batmon(device="bat_caravan", cell_index=0):
+    r = influxdb_client().query("""
+     SELECT mean(voltage_cell%03i) as u, mean(current) as i FROM "autogen"."batmon"      
+    WHERE time >= now() - 2d and time <= now() and device = '%s'
+     GROUP BY time(1s), "device"::tag, "cell_index"::tag fill(null)
+     """ % (cell_index, device))
+
+    points = r.get_points(tags=dict(device=device))
+    points = pd.DataFrame(points)
+    points.u.ffill(limit=20, inplace=True)
+    points.i.ffill(limit=200, inplace=True)
+    points.set_index(pd.DatetimeIndex(points.time), inplace=True)
+    points.drop(columns='time', inplace=True)
+    return points
