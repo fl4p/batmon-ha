f953e1b3b796f4399746892ac5d01792931da38a
Fabian <fabian@schlieper.email>
2023-11-10 15:50:50 +0000

    datasets

--- tools/impedance/datasets.py
+++ tools/impedance/datasets.py
@@ -3,7 +3,8 @@ import json
 import influxdb
 import pandas as pd
 
-from tools.impedance.data import ql_time_range
+from tools.impedance.cache import disk_cache_deco
+from tools.impedance.data import ql_time_range, fetch_batmon_ha_sensors
 
 _influxdb_client = None
 
@@ -55,3 +56,62 @@ def batmon(tr, device="bat_caravan", cell_index=0, freq="1s"):
     points.set_index(pd.DatetimeIndex(points.time), inplace=True)
     points.drop(columns='time', inplace=True)
     return points
+
+
+@disk_cache_deco()
+def daly22(num_cells, freq):
+    """
+    this is the first data i collected with 280ah lifepo4 from aliexpress
+
+    note#1: there is more data later the year (after a gap)
+    note#2: jbd22 is connected to the same battery (after daly bms)
+
+    :param num_cells:
+    :param freq:
+    :return:
+    """
+    df = fetch_batmon_ha_sensors(("2022-01-05", "2022-05-05"), 'daly_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
+@disk_cache_deco()
+def daly22_1(num_cells, freq):
+    """
+    this is the first data i collected with 280ah lifepo4 from aliexpress
+
+    note#1: there is more data later the year (after a gap)
+    note#2: jbd22 is connected to the same battery (after daly bms)
+
+    :param num_cells:
+    :param freq:
+    :return:
+    """
+    df = fetch_batmon_ha_sensors(("2022-01-05", "2022-05-05"), 'daly_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
+
+
+@disk_cache_deco()
+def daly22_full(num_cells, freq):
+    df = fetch_batmon_ha_sensors(("2022-01-05", "2022-12-01"), 'daly_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
+
+
+@disk_cache_deco()
+def daly22_idle(num_cells, freq):
+    df = fetch_batmon_ha_sensors(("2022-06-05", "2022-11-01"), 'daly_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
+
+
+@disk_cache_deco()
+def jbd22(num_cells, freq):
+    df = fetch_batmon_ha_sensors(("2022-01-05", "2022-05-05"), 'jbd_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
+
+@disk_cache_deco()
+def jbd22_full(num_cells, freq):
+    df = fetch_batmon_ha_sensors(("2022-01-05", "2022-12-01"), 'jbd_bms', num_cells=num_cells, freq=freq)
+    df.loc[:, 'i'] *= -1
+    return df
