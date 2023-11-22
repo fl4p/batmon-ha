826e1ccd5e994fe73a15e0c746e89ae46ad62085
Fabian <fabian@schlieper.email>
2023-11-22 16:59:20 +0000

    sampling
    publish on power peak
    re-connect after 20 errors
    fix discovery when voltages fetch fails

--- bmslib/sampling.py
+++ bmslib/sampling.py
@@ -98,6 +98,10 @@ class BmsSampler:
             if meter_state and meter.name in meter_state:
                 meter.restore(meter_state[meter.name]['reading'])
 
+        # self.power_stats = EWM(span=120, std_regularisation=0.1)
+        self._last_power = 0
+        self._t_last_power_jump = 0
+
     def get_meter_state(self):
         return {meter.name: dict(reading=meter.get()) for meter in self.meters}
 
@@ -135,6 +139,10 @@ class BmsSampler:
                 logger.warning('%s disconnect because no data has been flowing for some time', bms.name)
                 await bms.disconnect()
 
+            if bms.is_connected and self._num_errors > 20:
+                logger.warning("disconnecting %s due to too many errors %d", bms, self._num_errors)
+                await bms.disconnect()
+
             raise
 
     @mem_cache_deco(ttl=30)
@@ -150,6 +158,9 @@ class BmsSampler:
 
         was_connected = bms.is_connected
 
+        if not was_connected:
+            self._num_errors = 0
+
         t_conn = time.time()
 
         if not was_connected and t_conn < self._time_next_retry:
@@ -160,150 +171,175 @@ class BmsSampler:
         if not was_connected and not bms.is_virtual:
             logger.info('connecting bms %s', bms)
 
-            async with bms:
-                if not was_connected:
-                    logger.info('connected bms %s!', bms)
+        async with bms:
+            if not was_connected:
+                logger.info('connected bms %s!', bms)
 
-                if self.device_info is None and self.num_samples == 0:
-                    # try to fetch device info first. if bms.fetch() fails we might have at least some details
-                    await self._try_fetch_device_info()
+            if self.device_info is None and self.num_samples == 0:
+                # try to fetch device info first. if bms.fetch() fails we might have at least some details
+                await self._try_fetch_device_info()
 
-                t_fetch = time.time()
+            t_fetch = time.time()
 
-                sample = await bms.fetch()
+            sample = await bms.fetch()
 
-                t_now = time.time()
-                t_hour = t_now * (1 / 3600)
+            t_now = time.time()
+            t_hour = t_now * (1 / 3600)
 
-                if sample.timestamp < t_now - max(self.expire_after_seconds, MIN_VALUE_EXPIRY):
-                    raise SampleExpiredError("sample %s expired", sample.timestamp)
-                    # logger.warning('%s expired sample', bms.name)
-                    # return
+            if sample.timestamp < t_now - max(self.expire_after_seconds, MIN_VALUE_EXPIRY):
+                raise SampleExpiredError("sample %s expired", sample.timestamp)
+                # logger.warning('%s expired sample', bms.name)
+                # return
 
-                sample.num_samples = self.num_samples
+            sample.num_samples = self.num_samples
 
-                if self.current_calibration_factor and self.current_calibration_factor != 1:
-                    sample = sample.multiply_current(self.current_calibration_factor)
+            if self.current_calibration_factor and self.current_calibration_factor != 1:
+                sample = sample.multiply_current(self.current_calibration_factor)
 
-                # discharging P>0
-                self.power_integrator_charge += (t_hour, abs(min(0, sample.power)) * 1e-3)  # kWh
-                self.power_integrator_discharge += (t_hour, abs(max(0, sample.power)) * 1e-3)  # kWh
+            # discharging P>0
+            self.power_integrator_charge += (t_hour, abs(min(0, sample.power)) * 1e-3)  # kWh
+            self.power_integrator_discharge += (t_hour, abs(max(0, sample.power)) * 1e-3)  # kWh
 
-                if (self.sinks or self.bms_group) and not sample.temperatures:
-                    sample.temperatures = await self._fetch_temperatures_cached()
+            # self.power_stats.add(sample.power)
 
-                if self.bms_group:
-                    # update before invert current
-                    self.bms_group.update(bms, sample)
+            if (self.sinks or self.bms_group) and not sample.temperatures:
+                sample.temperatures = await self._fetch_temperatures_cached()
 
-                if self.invert_current:
-                    sample = sample.invert_current()
+            if self.bms_group:
+                # update before invert current
+                self.bms_group.update(bms, sample)
 
-                self.current_integrator += (t_hour, sample.current)  # Ah
-                self.power_integrator += (t_hour, sample.power * 1e-3)  # kWh
+            if self.invert_current:
+                sample = sample.invert_current()
 
-                self.cycle_integrator += (t_hour, sample.soc * (0.01 / 2))  # SoC 100->0 is a half cycle
-                self.charge_integrator += (t_hour, sample.charge)  # Ah
+            self.current_integrator += (t_hour, sample.current)  # Ah
+            self.power_integrator += (t_hour, sample.power * 1e-3)  # kWh
 
-                if self.algorithm:
-                    res = self.algorithm.update(sample)
-                    if res or self.bms.verbose_log:
-                        logger.info('Algo State=%s (bms=%s) -> %s ', self.algorithm.state,
-                                    BatterySwitches(**sample.switches), res)
+            self.cycle_integrator += (t_hour, sample.soc * (0.01 / 2))  # SoC 100->0 is a half cycle
+            self.charge_integrator += (t_hour, sample.charge)  # Ah
 
-                    if res:
-                        from bmslib.store import store_algorithm_state
-                        state = self.algorithm.state
-                        if state:
-                            store_algorithm_state(bms.name, algorithm_name=self.algorithm.name, state=state.__dict__)
+            if self.algorithm:
+                res = self.algorithm.update(sample)
+                if res or self.bms.verbose_log:
+                    logger.info('Algo State=%s (bms=%s) -> %s ', self.algorithm.state,
+                                BatterySwitches(**sample.switches), res)
 
-                    if res and res.switches:
-                        for swk in sample.switches.keys():
-                            if res.switches[swk] is not None:
-                                logger.info('%s algo set %s switch -> %s', bms.name, swk, res.switches[swk])
-                                await self.bms.set_switch('charge', res.switches[swk])
+                if res:
+                    from bmslib.store import store_algorithm_state
+                    state = self.algorithm.state
+                    if state:
+                        store_algorithm_state(bms.name, algorithm_name=self.algorithm.name, state=state.__dict__)
 
-                if self.num_samples == 0 and sample.switches and mqtt_client:
-                    logger.info("%s subscribing for %s switch change", bms.name, sample.switches)
-                    subscribe_switches(mqtt_client, device_topic=self.mqtt_topic_prefix, bms=bms,
-                                       switches=sample.switches.keys())
+                if res and res.switches:
+                    for swk in sample.switches.keys():
+                        if res.switches[swk] is not None:
+                            logger.info('%s algo set %s switch -> %s', bms.name, swk, res.switches[swk])
+                            await self.bms.set_switch('charge', res.switches[swk])
 
-                for sink in self.sinks:
-                    try:
-                        sink.publish_sample(bms.name, sample)
-                    except:
-                        logger.error(sys.exc_info(), exc_info=True)
+            if self.num_samples == 0 and sample.switches and mqtt_client:
+                logger.info("%s subscribing for %s switch change", bms.name, sample.switches)
+                subscribe_switches(mqtt_client, device_topic=self.mqtt_topic_prefix, bms=bms,
+                                   switches=sample.switches.keys())
 
-                self.downsampler += sample
+            for sink in self.sinks:
+                try:
+                    sink.publish_sample(bms.name, sample)
+                except:
+                    logger.error(sys.exc_info(), exc_info=True)
 
-                publish_discovery = (t_now - self._t_discovery) >= (60 * 10)
-                if publish_discovery:
-                    self._t_discovery = t_now
+            self.downsampler += sample
 
-                log_data = (t_now - self._last_time_log) >= (60 if self.num_samples < 1000 else 300) or bms.verbose_log
-                if log_data:
-                    self._last_time_log = t_now
+            publish_discovery = (t_now - self._t_discovery) >= (60 * 10)
+            if publish_discovery:
+                self._t_discovery = t_now
 
-                voltages = []
+            log_data = (t_now - self._last_time_log) >= (60 if self.num_samples < 1000 else 300) or bms.verbose_log
+            if log_data:
+                self._last_time_log = t_now
 
-                async def cached_fetch_voltages():
-                    nonlocal voltages
-                    if voltages:
-                        return voltages
+            voltages = []
 
-                    # TODO fetch_voltages at t_fetch interval and down-sampling?
+            async def cached_fetch_voltages():
+                nonlocal voltages
+                if voltages:
+                    return voltages
+
+                # TODO fetch_voltages at t_fetch interval and down-sampling?
+                try:
                     voltages = await bms.fetch_voltages()
 
                     if self.bms_group:
                         self.bms_group.update_voltages(bms, voltages)
+                except:
+                    logger.error("%s error fetching voltage", bms.name, exc_info=1)
+                    voltages = None
 
-                    return voltages
+                return voltages
 
-                if self.sinks:
-                    voltages = await cached_fetch_voltages()
-                    for sink in self.sinks:
-                        sink.publish_voltages(bms.name, voltages)
+            if self.sinks:
+                voltages = await cached_fetch_voltages()
+                for sink in self.sinks:
+                    sink.publish_voltages(bms.name, voltages)
 
-                if publish_discovery or not self.publish_period or (t_now - self._t_pub) >= self.publish_period:
-                    self._t_pub = t_now
+            # z_score = self.power_stats.z_score(sample.power)
+            # if abs(z_score) > 12:
+            #    logger.info('%s Power z_score %.1f (avg=%.0f std=%.2f last=%.0f)', bms.name, z_score, self.power_stats.avg.value, self.power_stats.stddev, sample.power)
 
-                    sample = self.downsampler.pop()
+            PWR_CHG_REG = 25  # regularisation to suppress changes when power is low
+            power_chg = (sample.power - self._last_power) / (abs(self._last_power) + PWR_CHG_REG)
+            if abs(power_chg) > 0.15 and abs(sample.power) > abs(self._last_power):
+                self._t_last_power_jump = t_now
+                if (t_now - self._t_pub) < self.publish_period:
+                    logger.info('%s Power jump %.0f %% (prev=%.0f last=%.0f, REG=%.0f)', bms.name, power_chg * 100,
+                                self._last_power, sample.power, PWR_CHG_REG)
+            self._last_power = sample.power
 
+            if publish_discovery or not self.publish_period or (t_now - self._t_pub) >= self.publish_period or \
+                    (t_now - self._t_last_power_jump) < 5:
+                self._t_pub = t_now
+
+                sample = self.downsampler.pop()
+
+                publish_sample(mqtt_client, device_topic=self.mqtt_topic_prefix, sample=sample)
+                log_data and logger.info('%s: %s', bms.name, sample)
+
+                self.publish_meters()
+
+                voltages = await cached_fetch_voltages()
+                publish_cell_voltages(mqtt_client, device_topic=self.mqtt_topic_prefix, voltages=voltages)
+
+                temperatures = sample.temperatures or await self._fetch_temperatures_cached()
+
+                if temperatures:
+                    publish_temperatures(mqtt_client, device_topic=self.mqtt_topic_prefix,
+                                         temperatures=temperatures)
+
+                if log_data and (voltages or temperatures) and not bms.is_virtual:
+                    logger.info('%s volt=[%s] temp=%s', bms.name, ','.join(map(str, voltages)),
+                                temperatures)
+
+            # publish home assistant discovery every 60 samples
+            if publish_discovery:
+                logger.info("Sending HA discovery for %s (num_samples=%d)", bms.name, self.num_samples)
+                if self.device_info is None:
+                    await self._try_fetch_device_info()
+                publish_hass_discovery(
+                    mqtt_client, device_topic=self.mqtt_topic_prefix,
+                    expire_after_seconds=self.expire_after_seconds,
+                    sample=sample,
+                    num_cells=len(voltages),
+                    temperatures=temperatures,
+                    device_info=self.device_info,
+                )
+
+                # publish sample again after discovery
+                if self.publish_period > 2:
+                    await asyncio.sleep(1)
                     publish_sample(mqtt_client, device_topic=self.mqtt_topic_prefix, sample=sample)
-                    log_data and logger.info('%s: %s', bms.name, sample)
 
-                    self.publish_meters()
-
-                    voltages = await cached_fetch_voltages()
-                    publish_cell_voltages(mqtt_client, device_topic=self.mqtt_topic_prefix, voltages=voltages)
-
-                    temperatures = sample.temperatures or await self._fetch_temperatures_cached()
-
-                    if temperatures:
-                        publish_temperatures(mqtt_client, device_topic=self.mqtt_topic_prefix,
-                                             temperatures=temperatures)
-
-                    if log_data and (voltages or temperatures) and not bms.is_virtual:
-                        logger.info('%s volt=[%s] temp=%s', bms.name, ','.join(map(str, voltages)),
-                                    temperatures)
-
-                # publish home assistant discovery every 60 samples
-                if publish_discovery:
-                    logger.info("Sending HA discovery for %s (num_samples=%d)", bms.name, self.num_samples)
-                    if self.device_info is None:
-                        await self._try_fetch_device_info()
-                    publish_hass_discovery(
-                        mqtt_client, device_topic=self.mqtt_topic_prefix,
-                        expire_after_seconds=self.expire_after_seconds,
-                        sample=sample,
-                        num_cells=len(voltages),
-                        temperatures=temperatures,
-                        device_info=self.device_info,
-                    )
-
-                self.num_samples += 1
-                t_disc = time.time()
-                self._t_wd_reset = t_disc
+        self.num_samples += 1
+        t_disc = time.time()
+        self._t_wd_reset = t_disc
 
         dt_conn = t_fetch - t_conn
         dt_fetch = t_disc - t_fetch
@@ -312,6 +348,8 @@ class BmsSampler:
                 dt_max > 0.01 and random.random() < (0.05 if sample.num_samples < 1e3 else 0.01)
                 and not bms.is_virtual and log_data):
             logger.info('%s times: connect=%.2fs fetch=%.2fs', bms, dt_conn, dt_fetch)
+
+        return sample
 
     def publish_meters(self):
         device_topic = self.mqtt_topic_prefix
