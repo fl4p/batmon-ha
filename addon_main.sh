71cccbbb0da30d061dd3819d552726d456aeee23
Fabian <fabian@schlieper.email>
2025-08-30 14:35:49 +0200

    continue main script on error

--- addon_main.sh
+++ addon_main.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/with-contenv bashio
 
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
+set +e # continue script on error
+
+# bashio::addon.option
+# bashio::config.exists
+# see https://github.com/hassio-addons/bashio/blob/main/lib/addons.sh
+
+if bashio::config.exists "install_newer_bleak"; then
+  bashio::addon.option "install_newer_bleak" # delete
+fi
+
+# query MQTT details from supervisor API
+MQTT_HOST=$(bashio::services mqtt "host" || true)
+MQTT_USER=$(bashio::services mqtt "username" || true)
+MQTT_PASSWORD=$(bashio::services mqtt "password" || true)
 
 /app/venv/bin/python3 main.py pair-only
 
