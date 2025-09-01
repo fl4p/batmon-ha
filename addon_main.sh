2086913d68806807454ade8958ec06360616b59e
Fabian <fabian@schlieper.email>
2025-09-01 10:29:00 +0200

    query mqtt port from supervisor API

--- addon_main.sh
+++ addon_main.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
-set +e # continue script on error
+# !/usr/bin/env bashio
+# !/usr/bin/with-contenv bashio
+# https://community.home-assistant.io/t/cannot-find-supervisor-token-environment-variable/543209/6
+# https://github.com/hassio-addons/bashio/
+
+# set +e # continue script on error
 
 # bashio::addon.option
 # bashio::config.exists
@@ -11,13 +16,19 @@ if bashio::config.exists "install_newer_bleak"; then
 fi
 
 # query MQTT details from supervisor API
-MQTT_HOST=$(bashio::services mqtt "host" || true)
-MQTT_USER=$(bashio::services mqtt "username" || true)
-MQTT_PASSWORD=$(bashio::services mqtt "password" || true)
+# see e.g. https://github.com/zigbee2mqtt/hassio-zigbee2mqtt/blob/master/common/rootfs/docker-entrypoint.sh
+MQTT_HOST="$(bashio::services 'mqtt' 'host')"
+MQTT_PORT="$(bashio::services 'mqtt' 'port')"
+MQTT_USER="$(bashio::services 'mqtt' 'username')"
+MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+
+bashio::log.blue "MQTT broker:     $MQTT_USER@$MQTT_HOST:$MQTT_PORT"
+# bashio::log.blue "SUPERVISOR_TOKEN:     $SUPERVISOR_TOKEN"
+
 
 /app/venv_bleak_pairing/bin/python3 main.py pair-only
 
 
-MQTT_HOST=$MQTT_HOST MQTT_USER=$MQTT_USER MQTT_PASSWORD=$MQTT_PASSWORD \
+MQTT_HOST="$MQTT_HOST" MQTT_PORT="$MQTT_PORT" MQTT_USER="$MQTT_USER" MQTT_PASSWORD="$MQTT_PASSWORD" \
   /app/venv/bin/python3 main.py
 
